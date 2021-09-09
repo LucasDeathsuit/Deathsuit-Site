f56be026b08035364294d065b654f344f64ee507
unknown <lucasdeathsuit@gmail.com>
2021-09-09 13:35:54 -0300

    Corrected images size on newComponent

--- src/components/newComponent/NewComponent.js
+++ src/components/newComponent/NewComponent.js
@@ -3,8 +3,8 @@ import formatText from '../../helpers/textFormatter'
 import css from "./newcomponent.module.css"
 
 export default function NewComponent({ noticia }) {
-    
-    if(!noticia) return null
+
+    if (!noticia) return null
 
     const anexos = noticia._embedded["wp:featuredmedia"]
 
@@ -12,13 +12,12 @@ export default function NewComponent({ noticia }) {
         <div className={css.newPanel}>
             <h1 className={css.title}>{noticia.title.rendered}</h1>
             <p className={css.text}>{formatText(noticia.content.rendered)}</p>
-            
+
             {
                 anexos.map(anexo => {
-                    return <img key={anexo.id} className={css.image} src={anexo.media_details.sizes.large.source_url} alt={anexo.alt_text}/>
+                    return <img key={anexo.id} className={css.image} src={anexo.media_details.sizes.medium_large.source_url} alt={anexo.alt_text} />
                 })
             }
-
         </div>
     )
 }

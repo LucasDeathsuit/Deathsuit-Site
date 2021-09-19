3c841e32a0512bf772f81cd79b491ab828ddb9d1
unknown <lucasdeathsuit@gmail.com>
2021-09-19 20:28:18 -0300

    Added Mobile Menu icon

--- src/components/navPanel/NavPanel.js
+++ src/components/navPanel/NavPanel.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavItem from '../navItem/NavItem'
 import css from './NavPanel.module.css'
+import MenuIcon from '@material-ui/icons/Menu';
 
 export default function NavPanel() {
+
+    const [isShowingMenu, setIsShowingMenu] = useState(false);
+
+    const handleMenu = () => {
+        setIsShowingMenu(!isShowingMenu)
+    }
+
     return (
         <nav className={css.navPanel}>
-            <ul className={css.items}>
-                <NavItem content='Home' link="/"></NavItem>
-                <NavItem content='News' link="/news"></NavItem>
-                <NavItem content='Contato' link="/contact-us"></NavItem>
-            </ul>
+            {/* Full NavBar */}
+            <div className={css.items}>
+                <MenuIcon className={css.menuIcon} onClick={handleMenu} />
+                <div className={css.leftRow}>
+                    <ul className={css.navItems}>
+                        <NavItem content='Home' link="/"></NavItem>
+                        <NavItem content='News' link="/news"></NavItem>
+                        <NavItem content='Contato' link="/contact-us"></NavItem>
+                    </ul>
+                </div>
+            </div>
+
+            {/* Mobile NavBar */}
+
             <ul className={`${css.items} ${css.rightRow}`}>
                 <NavItem icon="/Images/player.png" content="Ouça agora" link="https://www.youtube.com/watch?v=PV5YYa30QlE"></NavItem>
             </ul>
-        </nav>
+        </nav >
     )
 }
